9a005b83845faccc590d1df6d0e884c5fe82fdec
Morgan Cadow <morgan@ajarat.io>
2023-07-22 16:20:57 -0600

    Layout refresh and password protection

---
+++ src/middleware.ts
@@ -0,0 +1,46 @@
+import { defineMiddleware } from "astro/middleware";
+
+
+const VALID_USERNAME = 'mjc'
+const VALID_PASSWORD = 'designs'
+
+export const onRequest = defineMiddleware((context, next) => {
+
+    const { url, headers } = context.request
+    const { pathname } = new URL(url)
+
+    if (pathname === '/') {
+        return next() 
+    } 
+    
+    const creds = parseCredentials(headers)
+    if (creds && validateCredentials(creds)) {
+        return next()
+    }
+        
+    return new Response('', { status: 401, headers: {
+        'WWW-Authenticate': `Basic realm="Morgan's private portfolio"` 
+    }})
+});
+
+type Credentials = {
+    username: string
+    password: string
+}
+
+function validateCredentials(creds: Credentials): boolean {
+    return creds?.username === VALID_USERNAME && creds.password === VALID_PASSWORD
+}
+
+function parseCredentials(headers: Headers): Credentials | undefined {
+    try {
+        const creds = headers.get('authorization')?.replace(/^basic\s/i, '')
+        if (creds) {
+            const [username, password] = atob(creds).split(':')
+            return { username, password }
+        }        
+    } catch (e) {
+        return
+    }
+    
+}
